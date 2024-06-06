8b9f4d077ac85fbcc8a45da59f5e4ab4ca7f53f8
Pashtet <ptashkaaaaaa@gmail.com>
2024-06-07 00:35:54 +0200

    feat: create songs resource,songs dto, logger middleware

---
+++ src/songs/dto/create-song.dto.ts
@@ -0,0 +1,26 @@
+import {
+  IsArray,
+  IsDateString,
+  IsMilitaryTime,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+
+export class CreateSongDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly title: string;
+
+  @IsArray({})
+  @IsString({ each: true })
+  @IsNotEmpty()
+  readonly artists: string[];
+
+  @IsDateString()
+  @IsNotEmpty()
+  readonly releaseDate: Date;
+
+  @IsMilitaryTime()
+  @IsNotEmpty()
+  readonly duration: Date;
+}
